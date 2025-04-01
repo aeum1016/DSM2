37b996f83e7e0f9f4442b37cfe629d86899ceeca
Alex Eum <aeum1003@gmail.com>
2025-04-01 12:46:11 -0400

    Use env variable for env on backend.

--- serverGo/main.go
+++ serverGo/main.go
@@ -25,10 +25,18 @@ func main() {
 
 	config := cors.DefaultConfig()
 
-	config.AllowOrigins = []string{"http://localhost", os.Getenv("FRONTEND_URL")}
+	config.AllowOrigins = []string{"http://localhost:3000", os.Getenv("FRONTEND_URL")}
 	config.AllowCredentials = true
 	r.Use(cors.New(config))
 
 	routes.InitRoutes(r)
-	r.RunTLS(":8080", os.Getenv("SSL_CERT_CHAIN"), os.Getenv("SSL_CERT_PRIV"))
+
+	env := os.Getenv("ENV")
+	if(env == "DEV") {
+		r.Run(":8080")
+	} else if (env == "PROD") {
+		r.RunTLS(":8080", os.Getenv("SSL_CERT_CHAIN"), os.Getenv("SSL_CERT_PRIV"))
+	} else {
+		fmt.Println("No env defined")
+	}
 }
